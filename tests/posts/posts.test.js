c76e6445b2f37847d27626f0073e6dc5d1e257c0
anilpdvmsr <anil.palli@msr-it.com>
2019-09-24 04:22:32 -0700

    adding post route changes

--- tests/posts/posts.test.js
+++ tests/posts/posts.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../../server");
+const tokenGen = require("../helper/token.helper");
 
 jest.setTimeout(30000);
 describe("Posts Routes", () => {
@@ -27,6 +28,49 @@ describe("Posts Routes", () => {
     const res = await request(app)
       .get("/api/posts/" + "sejfaklfjl")
       .expect(404);
+    done();
+  });
+});
+
+describe("Posts post route /api/posts", () => {
+  // postive case
+  it("should return 200 status and post in response", async done => {
+    const valid_input = {
+      text:
+        "its symbolic nature in the woods, skeptically waking up to see the northen winds."
+    };
+    const token = await tokenGen();
+    const res = request(app)
+      .post(valid_input)
+      .expect(200);
+    done();
+  });
+  // negative case
+  it("should return 400 status", async done => {
+    const invalid_input = {
+      text: ""
+    };
+    const token = await tokenGen();
+    const res = await request(app)
+      .post("/api/posts")
+      .set("Authorization", token)
+      .send(invalid_input)
+      .expect(400);
+    done();
+  });
+
+  // negative case
+  it("should return 400 status", async done => {
+    const invalid_input = {
+      text: "fjalfjla"
+    };
+
+    const token = await tokenGen();
+    const res = await request(app)
+      .post("/api/posts")
+      .set("Authorization", token)
+      .send(invalid_input)
+      .expect(400);
     done();
   });
 });
